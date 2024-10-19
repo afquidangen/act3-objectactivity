32899bb765ba80186af8d2b5fb4828f824c4fc77
afquidangen <alyssafayequidangen@gmail.com>
2024-10-20 02:49:12 +0800

    Object Activity

---
+++ objectActivity.js
@@ -0,0 +1,10 @@
+let car = {type: "Sedan", model: "Honda City", color: "White"};
+console.log(typeof car);
+//output:
+//object
+
+car.wheels = 4;
+console.log(car);
+//output:
+// { type: 'Sedan', model: 'Honda City', color: 'White', wheels: 4 }
+
